70f3d64a671a5e552a150c88bf56c8d4f8186418
AnasImloul <imloul.anas@bcg.com>
2024-05-20 09:13:25 +0100

    Refactored build

--- B/fft.cpp
+++ B/fft.cpp
@@ -63,35 +63,32 @@ vector<ll> multiply(vector<ll> const& a, vector<ll> const& b) {
 }
 
 vector<ll> build(vector<ll>& nums) {
-    vector<ll> a(MAX), b(MAX << 1);
+    vector<ll> a(MAX), // count of each number i -> i
+               b(MAX << 1); // count of each duplicate pair i -> 2 * i
 
-    for (ll num : nums) a[num] += 1, b[2 * num] += 1;
+    for (ll num: nums) // populate the count of each number i
+        a[num] += 1, b[2 * num] += 1;
 
-    vector<ll> res = multiply(a, a); // generate all possible pairs
+    // generate all possible pairs (i, j)
+    vector<ll> res = multiply(a, a);
+    for (ll i: nums) // remove the repeated duplicates (i, i) -> 2 * i
+        res[2 * i] -= 1;
 
-    for (ll i : nums) res[2 * i] -= 1; // remove the case where i = j
+    // generate all possible triples (i, j, k) where i != j
+    res = multiply(res, a);
 
-    for (ll& i : res) i /= 2; // divide by 2 because we are counting (i, j) and (j, i)
+    // generate all possible triples (i, j, j)
+    vector<ll> repeat = multiply(a, b);
+    for (ll i: nums) // don't count the case where i = j (i, i, i) -> 3 * i
+        repeat[3 * i] -= 1;
 
-    res = multiply(res, a); // generate all possible triples (i, j, k) where i < j
-
-    vector<ll> repeat = multiply(a, b); // generate all possible triples (i, j, j)
-
-    for (ll i : nums) repeat[3 * i] -= 1; // don't count the case where i = j (i, i, i) to not remove it twice
-
-    for (int i = 0; i < repeat.size(); i++) res[i] -= repeat[i]; // remove the repeated duplicates (i, i, j)
-
-    for (ll& re : res) re = re / 3; // divide by 3 because we are counting (i, j, k) where k is free
+    // remove the repeated duplicates (i, i, j) & (j, j, i)
+    for (int i = 0; i < repeat.size(); i++)
+        res[i] -= 2 * repeat[i];
 
     return res;
 }
 
-vector<ll> prefix(vector<ll>& a) {
-    vector<ll> res(a.size() + 1, 0);
-    for (int i = 0; i < a.size(); i++)
-        res[i + 1] = res[i] + a[i];
-    return res;
-}
 
 int main() {
     cin.tie(nullptr);
@@ -106,13 +103,14 @@ int main() {
 
     vector<ll> count_a = build(a), count_b = build(b);
     count_b.resize(count_a.size(), 0);
-    vector<ll> prefix_b = prefix(count_b);
 
-    ll total_pa = n * (n - 1) * (n - 2) / 6 , total_pb = m * (m - 1) * (m - 2) / 6;
+    ll total_a = n * (n - 1) * (n - 2) , total_b = m * (m - 1) * (m - 2);
 
+    ll less_than = 0;
     double ans = 0;
     for (int i = 0; i <= 3 * MAX; i++) {
-        ans += (double) count_a[i] * (double) prefix_b[i] / ((double) total_pa * (double) total_pb);
+        ans += (double) count_a[i] * (double) less_than / ((double) total_a * (double) total_b);
+        less_than += count_b[i];
     }
 
     cout << fixed << setprecision(6) << ans << endl;
